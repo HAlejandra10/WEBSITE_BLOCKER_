cb16cabd0fcd43b46d0cf0bd44c3b827f6e24c3b
HeidyAPrimo <heidyprimo10@outlook.com>
2024-02-21 18:02:52 -0500

    create a function based to create components o widgets to style the view

--- bloqueador.py
+++ bloqueador.py
@@ -2,3 +2,16 @@ import tkinter as tk
 
 class Bloqueador():
     hostsFile = "C:/Windows/System32/drivers/etc/hosts"
+    
+    
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.geometry("400x150")
+        self.window.title("Bloqueador de Páginas")
+        self.window.resizable(width=False, height=False)
+        self.createLabel()
+        self.createEntry()
+        self.boton_crear()
+        self.boton_eliminar()
+        self.boton_listar()
+        self.window.mainloop()
