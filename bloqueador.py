b7ce3b707164eb769aca375e706492e5d857a7d4
HeidyAPrimo <heidyprimo10@outlook.com>
2024-02-21 18:08:36 -0500

    create a function into the class w/ instance(self) to can to access another atribbutes and methods of instance, adding widget

--- bloqueador.py
+++ bloqueador.py
@@ -15,3 +15,7 @@ class Bloqueador():
         self.boton_eliminar()
         self.boton_listar()
         self.window.mainloop()
+        
+    def createLabel(self):
+        self.label = tk.Label(self.window, text="Ingresa URL:")
+        self.label.pack(pady=5)
